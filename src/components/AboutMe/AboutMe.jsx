96ec5b67598ce0fee30dbc23951e5fb33cf64d2a
Tom Mach <57111473+tommach591@users.noreply.github.com>
2023-06-21 17:01:58 -0700

    portfolio and email added

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,4 +1,5 @@
 import "./AboutMe.css";
+import Resume from "../../assets/Tom_Mach_Resume.pdf";
 
 function AboutMe() {
   return (
@@ -14,6 +15,47 @@ function AboutMe() {
             published several web applications using React. I am always on the
             lookout for new projects to take on and further hone my skills.
           </h2>
+          <div className="Actions">
+            <a
+              className="ContactButton"
+              href={Resume}
+              download="Tom_Mach_Resume"
+            >
+              <h1>Download Resume</h1>
+              <img
+                src="https://api.iconify.design/material-symbols:download-rounded.svg?color=%23464646"
+                alt=""
+              />
+            </a>
+            <div
+              className="ContactButton"
+              onClick={() => {
+                window.open(
+                  "https://github.com/tommach591/my-portfolio",
+                  "_blank"
+                );
+              }}
+            >
+              <h1>Portfolio Github</h1>
+              <img
+                src="https://api.iconify.design/carbon:portfolio.svg?color=%23464646"
+                alt=""
+              />
+            </div>
+            <div
+              className="ContactButton"
+              onClick={() => {
+                navigator.clipboard.writeText("tom.m.mach@gmail.com");
+                alert("Email Copied!");
+              }}
+            >
+              <h1>Copy Email</h1>
+              <img
+                src="https://api.iconify.design/material-symbols:content-copy-outline.svg?color=%23464646"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
       <div className="Info">
